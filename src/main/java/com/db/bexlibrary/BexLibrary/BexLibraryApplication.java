0575f26b5e76772de4a677bf56fc537dc99ef9be
Alexandra Preda <predaalexanddra@gmail.com>
2018-04-18 21:58:36 +0300

    ceva

---
+++ src/main/java/com/db/bexlibrary/BexLibrary/BexLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.db.bexlibrary.BexLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BexLibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/db/bexlibrary/BexLibrary/BexLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.db.bexlibrary.BexLibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BexLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
